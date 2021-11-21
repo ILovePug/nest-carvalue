af615bcef0b80484fdd0925d111153807ffe5bf9
Qian Weng <qianweng@qians-mbp.lan>
2021-11-21 14:41:23 -0500

    basic module setup

---
+++ useful-command.sh
@@ -0,0 +1,14 @@
+# create new nest app
+nest new <project name>
+
+# create a new module. 
+# ex. `nest g module users` create file `users/users.module.ts`
+nest g module <module name>
+
+# create a new controller
+# ex. `nest g controller users` create file `users/users.controller.ts`
+nest g controller <controller name>
+
+# create a new service
+# ex. `nest g service users` create file `users/users.service.ts`
+nest g service <service name>
