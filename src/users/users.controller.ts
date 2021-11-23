5cca85af2c50af245af57c35fdcf6eca8d0cc37e
Qian Weng <qianweng@qians-mbp.lan>
2021-11-22 20:40:48 -0500

    create signup route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+@Controller('auth')
+export class UsersController {
+  constructor(private userSerivce: UsersService) {}
 
-@Controller('users')
-export class UsersController {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.userSerivce.create(body.email, body.password);
+  }
+}
