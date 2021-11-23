a8eed35c7be4033066d0e60c60b30e207220940f
Qian Weng <qianweng@qians-mbp.lan>
2021-11-22 20:41:05 -0500

    add some comments

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,7 +7,9 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // create the instance in memory
     const user = this.repo.create({ email, password });
+    // actually persist to the db
     return this.repo.save(user);
   }
 }
